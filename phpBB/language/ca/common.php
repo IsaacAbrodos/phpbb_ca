0a5a73a655fe2da3e9006abae2886f50836c1b50
Isaac Garcia Abrodos <isaac.abrodos@gmail.com>
2026-09-24 21:10:55 +0200

    Treure espai superflu al final de línia

--- phpBB/language/ca/common.php
+++ phpBB/language/ca/common.php
@@ -36,7 +36,7 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 //
-// Some characters you may want to copy&paste: 
+// Some characters you may want to copy&paste:
 // ’ » “ ” …
 //
 
